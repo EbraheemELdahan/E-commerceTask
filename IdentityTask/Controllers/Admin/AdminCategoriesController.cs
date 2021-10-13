0c085916599807950ac17779c3f5752c2c5b1071
EbraheemELdahan <ebraheem.dahan97@gmail.com>
2021-10-13 13:43:20 +0200

    create AdminCategory with pagination,search and filter

--- IdentityTask/Controllers/Admin/AdminCategoriesController.cs
+++ IdentityTask/Controllers/Admin/AdminCategoriesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using IdentityTask.Models;
+using PagedList;
 
 namespace IdentityTask.Controllers.Admin
 {
@@ -11,10 +12,34 @@ namespace IdentityTask.Controllers.Admin
     {
         ApplicationDbContext db = new ApplicationDbContext();
         // GET: AdminCategories
-        public ActionResult Index()
+        public ActionResult Index(int? page,string SortOrder)
         {
-            
-            return View(db.Categories.ToList());
+            var categories = db.Categories.ToList();
+            ViewBag.NameSortParam =String.IsNullOrEmpty(SortOrder)? "name_desc":"";
+            ViewBag.ProductsCountParam = SortOrder == "countProducts" ? "Prod_desc" : "countProducts";
+            switch (SortOrder)
+            {
+                case "name_desc":
+                    categories = db.Categories.OrderBy(a => a.Name).ToList();
+                    break;
+                case "countProducts":
+                    categories = db.Categories.OrderBy(a => a.Products.Count).ToList();
+                    break;
+                case "Prod_desc":
+                    categories = db.Categories.OrderByDescending(a => a.Products.Count).ToList();
+                    break;
+                default:
+                    categories = db.Categories.OrderBy(a => a.ID).ToList();
+                    break;
+
+
+
+            }
+
+            var pageNumber = page ?? 1;
+            var CategoriesPerPage = categories.ToPagedList(pageNumber,3);
+            ViewBag.CategoriesPerPage = CategoriesPerPage;
+            return View(CategoriesPerPage);
         }
         //Get
         public ActionResult Create()
@@ -37,8 +62,11 @@ namespace IdentityTask.Controllers.Admin
             }
             return View(category);
         }
-    
-           
+        public ActionResult Details(int id)
+        {
+            return View(db.Categories.Include("Products").FirstOrDefault(a => a.ID == id));
+        }
+
     }
     
 }
