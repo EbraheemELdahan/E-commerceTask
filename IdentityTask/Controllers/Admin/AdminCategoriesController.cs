85624d67f1266b7d23e60a8afd14d0f0508db96f
EbraheemELdahan <ebraheem.dahan97@gmail.com>
2021-10-13 12:43:49 +0200

    Create Category  and listed them

--- IdentityTask/Controllers/Admin/AdminCategoriesController.cs
+++ IdentityTask/Controllers/Admin/AdminCategoriesController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IdentityTask.Models;
 
 namespace IdentityTask.Controllers.Admin
 {
     public class AdminCategoriesController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: AdminCategories
         public ActionResult Index()
         {
+            
+            return View(db.Categories.ToList());
+        }
+        //Get
+        public ActionResult Create()
+        {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Description,CatName")]Category category,HttpPostedFileBase CatName)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                string NewImgUrl = category.ID + "." + CatName.FileName.Split('.')[1];
+                CatName.SaveAs(Server.MapPath("~/Images/CategoriesImages/" + NewImgUrl));
+                category.CatName = NewImgUrl;
+                db.SaveChanges();
+                return RedirectToAction("index", "AdminCategories");
+            }
+            return View(category);
+        }
+    
+           
     }
+    
 }
--- IdentityTask/Controllers/CategoriesController.cs
+++ IdentityTask/Controllers/CategoriesController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IdentityTask.Models;
 
 namespace IdentityTask.Controllers
 {
+
     public class CategoriesController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         // GET: Category
         public ActionResult Index()
         {
-            return View();
+            return View(db.Categories.ToList());
         }
     }
 }
